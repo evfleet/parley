0ecfeab2d790a00ba6080eacafdd07ecd94f7481
Evan Fleet <evfleet@gmail.com>
2024-02-05 13:08:40 -0500

    feat(api): build out api url structure

--- packages/api/src/modules/discussions/index.ts
+++ packages/api/src/modules/discussions/index.ts
@@ -6,8 +6,50 @@ export default async function (fastify: FastifyInstance) {
     const res = await fastify.db.execute(sql`SELECT NOW()`);
 
     return {
-      message: "Hello from Discussions",
+      message: "Test database",
       result: res,
     };
+  });
+
+  fastify.route<{
+    Params: { id: string };
+  }>({
+    method: "GET",
+    url: "/:id",
+    handler: async (request, reply) => {
+      const { id } = request.params;
+
+      return {
+        message: `Hello from discussion ${id}`,
+      };
+    },
+  });
+
+  fastify.route<{
+    Params: { id: string };
+  }>({
+    method: "GET",
+    url: "/:id/comments",
+    handler: async (request, reply) => {
+      const { id } = request.params;
+
+      return {
+        message: `Hello from discussion ${id} comments`,
+      };
+    },
+  });
+
+  fastify.route<{
+    Params: { discussionId: string; commentId: string };
+  }>({
+    method: "GET",
+    url: "/:discussionId/comments/:commentId",
+    handler: async (request, reply) => {
+      const { discussionId, commentId } = request.params;
+
+      return {
+        message: `Hello from iscussion ${discussionId} comment ${commentId}`,
+      };
+    },
   });
 }
